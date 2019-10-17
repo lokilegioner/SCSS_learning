2a814b5c3819359bc9fb4016d9a2a86b468c4a40
Alexey Komolov <lokilegioner@gmail.com>
2019-10-17 18:58:42 +0300

    some blocks and dropdown menu complete

--- js/code.js
+++ js/code.js
@@ -1,5 +1,37 @@
 let
-    dropBtnContainer = document.querySelectorAll(#drop-down-cage);
-    dropBtn = document.createElement();
+    listMenu = document.createElement('div'),
+    blockContent = document.getElementById('block-after-drop-menu'),
+    isDroped = false;
+    mt_afterDropedBlock = '9em';
+    mt_beforeDropedBlock = '3em';
+    listItems = document.querySelectorAll('.listBtn'),
+    menuItems = [],
+    dropBtnContainer = document.querySelector('#drop-down-cage'),
+    dropBtn = document.createElement('div');
 
-dropBtn.classlist.add('drop-menu-btn');
+listMenu.classList.add('drop-menu-js-hide');
+for (let i = 0; i < listItems.length; i++) {
+    menuItems.push(document.createElement('a'));
+    menuItems[i].setAttribute('href', '#');
+    menuItems[i].classList.add('listBtn');
+    menuItems[i].textContent = listItems[i].textContent;
+    listMenu.appendChild(menuItems[i]);
+}
+
+dropBtn.classList.add('drop-menu-btn');
+dropBtnContainer.appendChild(dropBtn);
+
+dropBtn.appendChild(listMenu);
+
+dropBtn.addEventListener('click', function(){
+    listMenu.classList.toggle('drop-menu-js-hide');
+    listMenu.classList.toggle('drop-menu-js');
+    if (!isDroped) {
+        blockContent.style.margin = mt_afterDropedBlock + ' auto';
+        isDroped = !isDroped;
+    } else {
+        blockContent.style.margin = '3em auto';
+        isDroped = !isDroped;
+    }
+    
+})
